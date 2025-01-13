42e213c9e67cd6f976d798db078292b3341aad66
valeriia <novikovavaleria994@gmail.com>
2025-01-13 18:03:55 +0100

    HabitsViewController: add add action

--- MyHabits/controllers/HabitViewController.swift
+++ MyHabits/controllers/HabitViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class HabitViewController: UIViewController {
+    
+    private(set) lazy var navigationBar: UINavigationBar = {
+        let navBar = UINavigationBar()
+        navBar.backgroundColor = UIColor(named: "NavigationBarBackground")
+        let closeButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(closeButtontapped))
+        let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveButtontapped))
+        let navItem = UINavigationItem(title: NSLocalizedString("add_new_habit", comment: ""))
+        navItem.leftBarButtonItem = closeButton
+        navItem.rightBarButtonItem = saveButton
+        navBar.setItems([navItem], animated: false)
+        navBar.translatesAutoresizingMaskIntoConstraints = false
+        return navBar
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        
+        view.addSubview(navigationBar)
+        let navigationBarConstraints = [
+            navigationBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            navigationBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            navigationBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        ]
+        NSLayoutConstraint.activate(navigationBarConstraints)
 
         // Do any additional setup after loading the view.
+    }
+    
+    @objc private func closeButtontapped() {
+        dismiss(animated: true)
+    }
+    
+    @objc private func saveButtontapped() {
+        print("save button tapped")
+        // Save new habit
+        // Close screen
     }
     
 
